537d1004a76bf5b48781e9e06bbaefbe1b83de1c
AbhishekArrawatia <arrawatia30061997@gmail.com>
2023-03-16 02:11:58 +0530

    Repository and auto mapper changes for Regions Controller

---
+++ NZWalks/NZWalks.API/Profiles/RegionProfile.cs
@@ -0,0 +1,13 @@
+﻿using NZWalks.API.DTO;
+using NZWalks.API.Models;
+using AutoMapper;
+namespace NZWalks.API.Profiles
+{
+    public class RegionProfile : Profile
+    {
+        public RegionProfile()
+        {
+            CreateMap<Region, RegionsDTO>();
+        }
+    }
+}
---
+++ NZWalks/NZWalks.API/Repositories/RegionRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+using NZWalks.API.Models;
+
+namespace NZWalks.API.Repositories
+{
+    public class RegionRepository : IRegionRepository
+    {
+        private readonly NZWalksDbContext _context;
+        public RegionRepository(NZWalksDbContext nZWalksDbContext)
+        {
+            _context = nZWalksDbContext;
+        }
+
+        
+
+        public async Task<IEnumerable<Region>> GetAllRegions()
+        {
+            return await _context.Regions.ToListAsync();
+        }
+    }
+}
